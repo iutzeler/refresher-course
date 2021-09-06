13cd476475ba7a74cb12c878361483fc974fc3ac
Franck Iutzeler <franck.iutzeler@gmail.com>
2021-09-06 17:31:17 +0200

    almost Python102

---
+++ lib/pyds.py
@@ -0,0 +1,12 @@
+######################################
+### 
+### the CLASS module
+###
+######################################
+
+
+teacherName = "Franck Iutzeler"
+teacherWebsite = "http://www.iutzeler.org"
+
+def printQuote():
+	print("The joy of coding Python should be in seeing short, concise, readable classes that express a lot of action in a small amount of clear code -- not in reams of trivial code that bores the reader to death.\n\n -- Guido van Rossum")
